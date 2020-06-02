1e0a9839aa70e291787a0135ccfe0bc76ec5321c
Leandro <leandro.c.santana@hotmail.com>
2020-06-02 13:40:45 -0300

    Criação da primeira rota: listagem de itens

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import knex from './database/connection'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/items', async (req, res) => {
+  const items = await knex('items').select('*')
+  
   return res.json({ message: 'Hello World' });
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import router from './router';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
